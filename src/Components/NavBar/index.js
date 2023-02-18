ec90a1ebc8dea46465baf2da646b6396b3255404
Joseph Hirotsu <jhirotsu@gmail.com>
2023-02-17 22:47:55 -0500

    sidebar,navbar,home

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import Paper from '@mui/material/Paper';
 
-function index() {
+function Index({open, setOpen, handleDrawerClose, handleDrawerOpen}) {
+  const [value, setValue] = useState(0);
+  const navigate = useNavigate();
+
+  const handleAddPage= () => {
+    navigate('/add')
+  }
+  const handleProfilePage = () => {
+    navigate('/profile')
+  }
+
+
+
+
   return (
-    <div>index</div>
-  )
+    <Box sx={{ pb: 7}} >
+      <CssBaseline />
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0,  zIndex: 13000}} elevation={3}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Meets"  icon={<CompareArrowsIcon />} onClick={handleDrawerOpen} />
+          <BottomNavigationAction label="New Meets" icon={<AddCircleOutlineOutlinedIcon />} 
+          onClick={handleAddPage} />
+          <BottomNavigationAction label="Profile" icon={<PermIdentityIcon />} onClick={handleProfilePage}/>
+        </BottomNavigation>
+      </Paper>
+    </Box>
+  );
+
 }
 
-export default index
+export default Index
--- src/Components/SideBar/index.js
+++ src/Components/SideBar/index.js
@@ -1,9 +1,139 @@
-import React from 'react'
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+// import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+// import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+// import EmojiEmotionsOutlinedIcon from '@mui/icons-material/MoveToInbox';
+import MeetSafeLogo from '../../Assets/MeetSafeLogo2.png'
+import { useNavigate } from 'react-router-dom';
+import ClickAwayListener from '@mui/base/ClickAwayListener';
 
-function index() {
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+
+
+const drawerWidth = 150;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
+
+// Avatar config
+  function stringToColor(string) {
+    let hash = 0;
+    let i;
+
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = '#';
+
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+
+    return color;
+  }
+
+
+// End Avatar Config 
+
+export default function Index({open, setOpen, handleDrawerClose, handlerDraweOpen}) {
+  // const theme = useTheme();
+  const navigate = useNavigate();
+  const handleHome = () => {
+    navigate("/home");
+  };
+
+  const handleClickAway = () => {
+    if (open) {
+      setOpen(false);
+    }
+  };
+  console.log(open, "SideBar/AppBar.js");
+
+
   return (
-    <div>index</div>
-  )
-}
+    // <ClickAwayListener onClickAway={handleClickAway} mouseEvent="onMouseDown" touchEvent="onTouchStart">
+    //   <div>
+    <Box sx={{ display: "flex", zindex: -6 }}>
+      <CssBaseline />
 
-export default index
+      <Drawer id="Drawer" variant="permanent" open={open}>
+        <DrawerHeader id="DrawerHeader" sx={{alignItems:'center', justifyContent:'center'}}>
+          <IconButton onClick={handleDrawerClose} >
+            {open ? (
+              <ChevronLeftIcon sx={{ fontSize: "35px" }} />
+            ) : (
+              <img src={MeetSafeLogo} alt="logo" onClick={handleHome} />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+
+        <IconButton onClick={handleHome} sx={{left: 0, bottom: 0,  opacity: open ? 1 : 0}}>
+          <img src={MeetSafeLogo} alt="logo" />
+        </IconButton>
+      </Drawer>
+    </Box>
+    // </div>
+    // </ClickAwayListener>
+  );
+}
