f96ce0a68e63b46d82786154aa0003f833f08f72
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2023-02-17 23:16:44 -0500

    adding GMap component

---
+++ src/Components/GMap/Index.js
@@ -0,0 +1,152 @@
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  InfoWindow,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
+import { useEffect, useState } from "react";
+
+function GMap({
+  setMap,
+  origin,
+  originAddress,
+  midPoint,
+  width,
+  height,
+  nearby,
+}) {
+  const [center, setCenter] = useState(origin);
+  const [selectedMarker, setSelectedMarker] = useState(null);
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+
+  useEffect(() => {
+    setDirectionsResponse(null);
+    setDistance(null);
+    setDuration(null);
+    setSelectedMarker(null);
+  }, [nearby]);
+
+  useEffect(() => {
+    if (midPoint) {
+      setCenter(midPoint);
+    } else {
+      setCenter(origin);
+    }
+  }, [origin, midPoint]);
+
+  const iconUrl = {
+    police:
+      "https://img.icons8.com/external-nawicon-flat-nawicon/64/null/external-police-station-location-nawicon-flat-nawicon.png",
+    fire_station:
+      "https://img.icons8.com/external-tulpahn-outline-color-tulpahn/64/null/external-fire-station-building-tulpahn-outline-color-tulpahn.png",
+    hospital: "https://img.icons8.com/dusk/64/null/hospital-2.png",
+  };
+
+  const options = {
+    disableDefaultUI: true,
+    zoomControl: true,
+  };
+
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+    libraries: ["places"],
+  });
+
+  const showMarkers = nearby.data
+    ? nearby.data.slice(0, 5).map((marker) => (
+        <Marker
+          key={marker.place_id}
+          vicinity={marker.vicinity}
+          position={{
+            lat: marker.geometry.location.lat,
+            lng: marker.geometry.location.lng,
+          }}
+          icon={{
+            url: iconUrl[nearby.type],
+            scaledSize: new window.google.maps.Size(30, 30),
+            origin: new window.google.maps.Point(0, 0),
+            anchor: new window.google.maps.Point(15, 15),
+          }}
+          animation={window.google.maps.Animation.DROP}
+          onClick={() => {
+            setSelectedMarker(marker);
+            setCenter({
+              lat: marker.geometry.location.lat,
+              lng: marker.geometry.location.lng,
+            });
+            calculateRoute(marker.vicinity);
+          }}
+        />
+      ))
+    : null;
+
+  // directions
+  async function calculateRoute(destination) {
+    if (originAddress === "" || destination === "") {
+      return;
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService();
+    const results = await directionsService.route({
+      origin: originAddress,
+      destination: destination,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+    setDirectionsResponse(results);
+    setDistance(results.routes[0].legs[0].distance.text);
+    setDuration(results.routes[0].legs[0].duration.text);
+  }
+
+  if (loadError) return <div>Error Loading map!</div>;
+  if (!isLoaded) return <div>Loading...</div>;
+
+  return (
+    <GoogleMap
+      onLoad={(map) => setMap(map)}
+      zoom={13}
+      center={center}
+      mapContainerStyle={{ width: width, height: height }}
+      options={options}
+    >
+      {origin ? <Marker position={origin} /> : null}
+      {}
+      {showMarkers}
+      {selectedMarker ? (
+        <InfoWindow
+          position={{
+            lat: selectedMarker.geometry.location.lat,
+            lng: selectedMarker.geometry.location.lng,
+          }}
+          onCloseClick={() => setSelectedMarker(null)}
+          onUnmount={() => setSelectedMarker(null)}
+        >
+          <div>
+            <p>{selectedMarker.name}</p>
+            <p>{selectedMarker.vicinity}</p>
+            {selectedMarker.opening_hours ? (
+              selectedMarker.opening_hours.open_now ? (
+                <p>Currently Open.</p>
+              ) : (
+                <p>Closed</p>
+              )
+            ) : null}
+            {distance ? <p>Distance: {distance}</p> : null}
+            {duration ? <p>Time needed: {duration}</p> : null}
+            <button className="text-blue-600 underline" onClick={() => {}}>
+              Select Location
+            </button>
+          </div>
+        </InfoWindow>
+      ) : null}
+      {directionsResponse && (
+        <DirectionsRenderer directions={directionsResponse} />
+      )}
+    </GoogleMap>
+  );
+}
+
+export default GMap;
