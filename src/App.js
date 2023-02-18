48266002a65070eb926a360c4393aa02f2bcb7a3
Joseph Hirotsu <jhirotsu@gmail.com>
2023-02-17 22:12:40 -0500

    edit index.html, app.js, index.js

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import SignUp from './Pages/SignUp'
+import Profile from './Pages/Profile'
+import AddExchange from './Pages/AddExchange'
+import Exchanges from './Pages/Exchanges'
+import History from './Pages/History'
+import Notifications from './Pages/Notifications'
+
+function App() {
+  
+  return (
+    <Routes>
+      <Route path="/#" element={<Home/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/*" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/add" element={<AddExchange/>}/>
+      <Route path="/exchanges" element={<Exchanges/>}/>
+      <Route path="/history" element={<History/>}/>
+      <Route path="/notifications" element={<Notifications/>}/>
+    </Routes>
+  );
+}
+
+export default App;
