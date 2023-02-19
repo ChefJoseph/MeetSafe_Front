fd658a9cec78dd0d1d43f310cf355aaa23afa969
alextaylor <alextaylor515@gmail.com>
2023-02-19 01:37:14 -0500

    Added patch for user and fill address with current location

--- src/Pages/AddExchange/AddForm.js
+++ src/Pages/AddExchange/AddForm.js
@@ -14,6 +14,8 @@ import useMidPointFinder from "../../CustomHooks/useMidPointFinder";
 import { Autocomplete } from "@react-google-maps/api";
 import { UserContext } from "../../Context/UserContext";
 import { MapContext } from "../../Context/MapContext";
+import Grid from "@mui/material/Grid";
+// import { Typography } from "@mui/material/styles/createTypography";
 
 function AddForm() {
   const { currentUser } = useContext(UserContext);
@@ -137,118 +139,125 @@ function AddForm() {
   console.log(error, "addform time error");
 
   return (
-    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-      {isLoaded ? (
-        <Box
-          component="main"
-          sx={{ display: "flex", flexDirection: "column", gap: 2, my: 2 }}
-        >
-          <h1>Create an invite</h1>
-          {/* <TextField
-            ref={partyRef}
-            label="Username"
-          /> */}
-          <input
-            ref={partyRef}
-            style={{ width: "100%", height: "50px", borderRadius: "5px" }}
-            placeholder={"Username"}
-          ></input>
-          <Box>
-            <Button colorScheme="pink" type="submit" onClick={addParty}>
-              Add party
-            </Button>
-          </Box>
-          {/* <TextField
-            ref={descriptionRef}
-            label="Description"
-            multiline
-            rows={3}
-          /> */}
-          <textarea
-            ref={descriptionRef}
-            style={{ width: "100%", height: "50px", borderRadius: "5px" }}
-            placeholder={"Description"}
-          ></textarea>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        {isLoaded ? (
 
-          <LocalizationProvider dateAdapter={AdapterDayjs} sx={{ mt: 2 }}>
-            <Stack spacing={0}>
-              <MobileDatePicker
-                label="Date"
-                value={dateValue}
-                onChange={(newValue) => {
-                  setDateValue(newValue);
-                }}
-                renderInput={(params) => <TextField {...params} />}
-                minDate={dayjs(Date.now())}
-                onError={(newError) => {
-                  setError(newError);
-                  console.log(newError);
-                }}
-              />
-              <br />
-              <MobileTimePicker
-                label="Time"
-                value={timeValue}
-                onChange={(newValue) => {
-                  setTimeValue(newValue);
-                }}
-                renderInput={(params) => <TextField {...params} />}
-                minTime={dayjs(Date.now())}
-                onError={(newError) => setError(newError)}
-              />
-            </Stack>
-          </LocalizationProvider>
-          <Box flexGrow={1} sx={{ marginTop: 1 }}>
-            <Autocomplete id="autocomplete">
+          <Box
+            component="main"
+            sx={{ display: "flex", flexDirection: "column", gap: 2, my: 2 }}
+          >
+            <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <h1>Create an invite</h1>
               {/* <TextField
-                ref={originRef}
-                label="Your address"
-                sx={{ width: "100%" }}
+                ref={partyRef}
+                label="Username"
               /> */}
               <input
-                ref={originRef}
+                ref={partyRef}
                 style={{ width: "100%", height: "50px", borderRadius: "5px" }}
-                placeholder={"Your Address"}
+                placeholder={"Username"}
               ></input>
-            </Autocomplete>
+              <Box >
+                <Button colorScheme="pink" type="submit" onClick={addParty}>
+                  Add party
+                </Button>
+              </Box>
+              {/* <TextField
+                ref={descriptionRef}
+                label="Description"
+                multiline
+                rows={3}
+              /> */}
+              <textarea
+                ref={descriptionRef}
+                style={{ width: "100%", height: "50px", borderRadius: "5px" }}
+                placeholder={"Description"}
+              ></textarea>
+
+              <LocalizationProvider dateAdapter={AdapterDayjs} sx={{ mt: 2 }}>
+                <Stack spacing={0}>
+                  <MobileDatePicker
+                    label="Date"
+                    value={dateValue}
+                    onChange={(newValue) => {
+                      setDateValue(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                    minDate={dayjs(Date.now())}
+                    onError={(newError) => {
+                      setError(newError);
+                      console.log(newError);
+                    }}
+                  />
+                  <br />
+                  <MobileTimePicker
+                    label="Time"
+                    value={timeValue}
+                    onChange={(newValue) => {
+                      setTimeValue(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                    minTime={dayjs(Date.now())}
+                    onError={(newError) => setError(newError)}
+                  />
+                </Stack>
+              </LocalizationProvider>
+              <Box flexGrow={1} sx={{ marginTop: 1 }}>
+                <Autocomplete id="autocomplete">
+                  {/* <TextField
+                    ref={originRef}
+                    label="Your address"
+                    sx={{ width: "100%" }}
+                  /> */}
+                  <input
+                    ref={originRef}
+                    style={{ width: "100%", height: "50px", borderRadius: "5px" }}
+                    placeholder={"Your Address"}
+                  ></input>
+                </Autocomplete>
+              </Box>
+              <Box>
+                <Button type="submit" onClick={updateOrigin}>
+                  Update Origin
+                </Button>
+              </Box>
+              {/* <Box>
+                {meetAddress ? (
+                  <>
+                    <p>Meeting address: </p>
+                    <p>{meetAddress.meeting_address}</p>
+                  </>
+                ) : (
+                  <Typography>Please select a meeting location:</Typography>
+                )}
+              </Box> */}
+            </Grid>
+            <Grid item xs={12}>
+              <Box>
+                <AddFormMapHolder
+                  map={map}
+                  setMap={setMap}
+                  origin={startCoor}
+                  originAddress={address1}
+                  midPoint={midPoint}
+                  nearby={nearby}
+                  setNearby={setNearby}
+                  setMeetAddress={setMeetAddress}
+                />
+              </Box>
+            </Grid>
+              <Box>
+                <Button variant="contained" onClick={sendInvite}>
+                  Send Invite
+                </Button>
+              </Box>
+            </Grid>
           </Box>
-          <Box>
-            <Button type="submit" onClick={updateOrigin}>
-              Update Origin
-            </Button>
-          </Box>
-          <Box>
-            {meetAddress ? (
-              <>
-                <p>Meeting address: </p>
-                <p>{meetAddress.meeting_address}</p>
-              </>
-            ) : (
-              <Typography>Please select a meeting location:</Typography>
-            )}
-          </Box>
-          <Box>
-            <AddFormMapHolder
-              map={map}
-              setMap={setMap}
-              origin={startCoor}
-              originAddress={address1}
-              midPoint={midPoint}
-              nearby={nearby}
-              setNearby={setNearby}
-              setMeetAddress={setMeetAddress}
-            />
-          </Box>
-          <Box>
-            <Button variant="contained" onClick={sendInvite}>
-              Send Invite
-            </Button>
-          </Box>
-        </Box>
-      ) : (
-        <h1>Loading...</h1>
-      )}
-    </Box>
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </Box>  
   );
 }
 
--- src/Pages/Profile/ProfileInfo.js
+++ src/Pages/Profile/ProfileInfo.js
@@ -1,19 +1,91 @@
 import React, { useEffect } from "react";
 import { Box } from "@mui/material";
 import AddressUpdateForm from "./AddressUpdateForm";
+import TextField from '@mui/material/TextField';
 import { useState } from 'react';
+import Button from "@mui/material/Button";
 
 
-function ProfileInfo({currentUser}) {
+function ProfileInfo({currentUser,edit}) {
+
+  function handleChangeDetails(event) {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    fetch("/users",{method:"PATCH",body:data}).then(resp=>resp.json())
+  }
 
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+      { !edit ?
+      <>
       <h1>Profile</h1>
       <h4>{currentUser?.username}</h4>
       <h4>{currentUser?.email}</h4>
-      <h4>{currentUser?.username}</h4>
+      <h4>{currentUser?.phone}</h4>
       <h4>{currentUser?.address} {currentUser?.state} {currentUser?.zipcode}</h4>
       <AddressUpdateForm />
+      </>
+        :
+      <Box component="form">
+      <TextField
+                defaultValue = {currentUser?.username}
+                margin="normal"
+                required
+                fullWidth
+                name="username"
+                label="Username"
+                type="text"
+                id="username"
+                autoComplete="username"
+                autoFocus
+              />
+       <TextField
+              defaultValue = {currentUser?.email}
+                margin="normal"
+                required
+                fullWidth
+                name="email"
+                label="Email"
+                type="text"
+                id="email"
+                autoComplete="email"
+                autoFocus
+              />
+       <TextField
+                defaultValue = {currentUser?.phone}
+                margin="normal"
+                required
+                fullWidth
+                name="phone"
+                label="Phone"
+                type="phone"
+                id="phone"
+                autoComplete="phone"
+                autoFocus
+              />
+       <TextField
+                defaultValue = {currentUser?.address}
+                margin="normal"
+                required
+                fullWidth
+                name="address"
+                label="Address"
+                type="text"
+                id="address"
+                autoComplete="address"
+                autoFocus
+              />
+        <Button type="submit"
+                fullWidth
+                variant="contained" 
+                onSubmit={handleChangeDetails}>
+                Change
+        </Button>
+
+      </Box>
+      }
+
     </Box>
   );
 }
