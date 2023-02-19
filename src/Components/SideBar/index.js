fe7b8e60883b5949c710665bfb9fd088a5031d6d
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2023-02-19 00:34:08 -0500

    adding meeting invite cycle

--- src/Components/SideBar/index.js
+++ src/Components/SideBar/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from "react";
-import ExchangeContext from '../../ExchangeProvider'
+import ExchangeContext from "../../ExchangeProvider";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import MuiDrawer from "@mui/material/Drawer";
@@ -13,12 +13,12 @@ import ListItemButton from "@mui/material/ListItemButton";
 // import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 // import EmojiEmotionsOutlinedIcon from '@mui/icons-material/MoveToInbox';
-import MeetSafeLogo from '../../Assets/MeetSafeLogo2.png'
-import { useNavigate } from 'react-router-dom';
-import ClickAwayListener from '@mui/base/ClickAwayListener';
+import MeetSafeLogo from "../../Assets/MeetSafeLogo2.png";
+import { useNavigate } from "react-router-dom";
+import ClickAwayListener from "@mui/base/ClickAwayListener";
 
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
 
 const drawerWidth = 150;
 
@@ -52,7 +52,6 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
 
-
 const Drawer = styled(MuiDrawer, {
   shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
@@ -72,43 +71,49 @@ const Drawer = styled(MuiDrawer, {
 }));
 
 // Avatar config
-  function stringToColor(string) {
-    let hash = 0;
-    let i;
+function stringToColor(string) {
+  let hash = 0;
+  let i;
 
-    /* eslint-disable no-bitwise */
-    for (i = 0; i < string.length; i += 1) {
-      hash = string.charCodeAt(i) + ((hash << 5) - hash);
-    }
-
-    let color = '#';
-
-    for (i = 0; i < 3; i += 1) {
-      const value = (hash >> (i * 8)) & 0xff;
-      color += `00${value.toString(16)}`.slice(-2);
-    }
-    /* eslint-enable no-bitwise */
-
-    return color;
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
   }
 
-  // function stringAvatar(name) {
-  //   return {
-  //     sx: {
-  //       bgcolor: stringToColor(name),
-  //     },
-  //     children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
-  //   };
-  // }
-// End Avatar Config 
+  let color = "#";
 
-export default function Index({open, setOpen, handleDrawerClose, handlerDraweOpen}) {
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+// function stringAvatar(name) {
+//   return {
+//     sx: {
+//       bgcolor: stringToColor(name),
+//     },
+//     children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+//   };
+// }
+// End Avatar Config
+
+export default function Index({
+  open,
+  setOpen,
+  handleDrawerClose,
+  handlerDraweOpen,
+}) {
   // const theme = useTheme();
   const navigate = useNavigate();
   const handleHome = () => {
     navigate("/home");
   };
-  const { exchanges, setExchanges, selectExchange, selectedExchange } = useContext(ExchangeContext);
+  const { exchanges, setExchanges, selectExchange, selectedExchange } =
+    useContext(ExchangeContext);
 
   const handleClickAway = () => {
     if (open) {
@@ -118,7 +123,7 @@ export default function Index({open, setOpen, handleDrawerClose, handlerDraweOpe
 
   function handleSelectExchange(ExchangeId) {
     selectExchange(ExchangeId);
-    navigate("/exchange/"+ExchangeId)
+    navigate("/exchange/" + ExchangeId);
   }
   console.log(open, "SideBar/AppBar.js");
 
@@ -131,17 +136,17 @@ export default function Index({open, setOpen, handleDrawerClose, handlerDraweOpe
   //   };
 
   useEffect(() => {
-    fetch('/exchanges')
-      .then((res) => {
-        if (res.ok) {
-          res.json()
-            .then((data) => {
-              setExchanges(data)
-              // console.log(data, "sidebar exchanges")
-            });
-        }
-      });
+    fetch("/exchanges").then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          setExchanges(data);
+          // console.log(data, "sidebar exchanges")
+        });
+      }
+    });
   }, []);
+
+  console.log(exchanges);
 
   return (
     // <ClickAwayListener onClickAway={handleClickAway} mouseEvent="onMouseDown" touchEvent="onTouchStart">
@@ -150,8 +155,11 @@ export default function Index({open, setOpen, handleDrawerClose, handlerDraweOpe
       <CssBaseline />
 
       <Drawer id="Drawer" variant="permanent" open={open}>
-        <DrawerHeader id="DrawerHeader" sx={{alignItems:'center', justifyContent:'center'}}>
-          <IconButton onClick={handleDrawerClose} >
+        <DrawerHeader
+          id="DrawerHeader"
+          sx={{ alignItems: "center", justifyContent: "center" }}
+        >
+          <IconButton onClick={handleDrawerClose}>
             {open ? (
               <ChevronLeftIcon sx={{ fontSize: "35px" }} />
             ) : (
@@ -160,53 +168,75 @@ export default function Index({open, setOpen, handleDrawerClose, handlerDraweOpe
           </IconButton>
         </DrawerHeader>
         <Divider />
-        
-        <Stack id="AvatarStack" direction="column" spacing={1} sx={{ display: "flex",justifyContent:'center', alignItems: "center", mt:1, mb:1 }}>
+
+        <Stack
+          id="AvatarStack"
+          direction="column"
+          spacing={1}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            mt: 1,
+            mb: 1,
+          }}
+        >
           {/* Sort and then map */}
-          {exchanges?.sort((a,b)=> {
-            const datea = new Date(a)
-            const dateb = new Date(b)
+          {exchanges
+            ?.sort((a, b) => {
+              const datea = new Date(a);
+              const dateb = new Date(b);
 
-            const amil = datea.getTime()
-            const bmil = dateb.getTime()
+              const amil = datea.getTime();
+              const bmil = dateb.getTime();
 
-            if (amil > bmil) {
-              return 1
-            }
-            if (amil < bmil) {
-              return -1
-            }
-            return 0 
-
-          }).map( exchange => (
-            <ListItem key={exchange.id} 
-            disablePadding 
-            sx={{ display: 'block' }}
-            selected={exchange.id === selectedExchange}
-            onClick={() => handleSelectExchange(exchange.id)}
-            >
-              <ListItemButton
-                sx={{
-                  justifyContent:'center', 
-                }}
+              if (amil > bmil) {
+                return 1;
+              }
+              if (amil < bmil) {
+                return -1;
+              }
+              return 0;
+            })
+            .map((exchange) => (
+              <ListItem
+                key={exchange.id}
+                disablePadding
+                sx={{ display: "block" }}
+                selected={exchange.id === selectedExchange}
+                onClick={() => handleSelectExchange(exchange.id)}
               >
-                <Avatar key={exchange.id} 
-                sx={{ bgcolor: stringToColor(`${exchange.user[1].username}`) }}>
-                  {exchange.user[1].username.split(",")[0][0].toUpperCase()}
-                </Avatar>
-                <ListItemText primary={exchange.user[1].username.toUpperCase()} sx={{ textAlign: 'center', opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+                <ListItemButton
+                  sx={{
+                    justifyContent: "center",
+                  }}
+                >
+                  <Avatar
+                    key={exchange.id}
+                    sx={{
+                      bgcolor: stringToColor(`${exchange.user[1].username}`),
+                    }}
+                  >
+                    {exchange.user[1].username.split(",")[0][0].toUpperCase()}
+                  </Avatar>
+                  <ListItemText
+                    primary={exchange.user[1].username.toUpperCase()}
+                    sx={{ textAlign: "center", opacity: open ? 1 : 0 }}
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
         </Stack>
 
         <Divider />
 
-        <IconButton onClick={handleHome} sx={{left: 0, bottom: 0,  opacity: open ? 1 : 0}}>
+        <IconButton
+          onClick={handleHome}
+          sx={{ left: 0, bottom: 0, opacity: open ? 1 : 0 }}
+        >
           <img src={MeetSafeLogo} alt="logo" />
         </IconButton>
       </Drawer>
-
     </Box>
     // </div>
     // </ClickAwayListener>
