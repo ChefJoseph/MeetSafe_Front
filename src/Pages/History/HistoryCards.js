f428bfefdf0e75a301c603490350a34ec3ff88ca
Hammad Faiz <hammadfaiz172@gmail.com>
2023-02-18 21:47:55 -0500

    fetched date for user exchange history in HistoryCard.js

--- src/Pages/History/HistoryCards.js
+++ src/Pages/History/HistoryCards.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, {useState , useEffect} from 'react'
+
 
 function HistoryCards() {
+  const [userExchanges, setUserExchanges] = useState(null)
+
+  useEffect(() => {
+    fetch('/history').then(res => res.json()).then(res => setUserExchanges(res))
+  }, []);
+  
   return (
     <div>
         HistoryCards
